050b86839ea2a50257188d10717d514b3199451a
Ruslan <ruslan56781@mail.ru>
2023-03-21 14:02:11 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple Blockchain Python 
+
+## Build / Run
+
+```shell
+git clone https://github.com/QuitM6n/Blockchain.git
+cd Blockchain
+
+docker build -t blockchain .
+docker run -d -p 8080:8080 --name blockchain-python blockchain 
+```
+
+## Endpoints
+
+### - NewTransaction `/new/transaction`
+
+### - GetBlock  `/get/block`
+
---
+++ blockchain.py
@@ -0,0 +1,54 @@
+from time import time
+import json
+import hashlib
+
+
+class Blockhain(object):
+    def __init__(self):
+        self.chain = []
+        self.transaction = []
+        self.create_block(proof=100, previous_hash=1)
+
+    def create_block(self, proof, previous_hash=None):
+        block = {'index': len(self.chain)+1,
+                 'timestamp': time(),
+                 'transaction': self.transaction,
+                 'proof': proof,
+                 'previous_hash': previous_hash,
+                 }
+
+        self.current_transactions = []
+        self.chain.append(block)
+
+        return block
+
+    def new_transaction(self, sender, recipient, amount):
+        self.transaction.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+        })
+
+        return self.get_previous_block['index']+1
+    
+    @property
+    def get_previous_block(self):
+        return self.chain[-1]
+    
+    @staticmethod
+    def hash_block(block):
+        to_hash = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(to_hash).hexdigest
+
+    def proof_of_work(self, last_proof):
+        proof = 0
+        while self.block_IsValid(last_proof, proof) is False:
+            proof += 1
+        return proof
+    
+    @staticmethod
+    def block_IsValid(last_proof, proof):
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
